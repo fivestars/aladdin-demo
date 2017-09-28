b7b149de3784e3a940ade90969891b3d4bf07e12
Hope Yu <hope.yu@fivestars.com>
2017-09-28 15:28:15 -0700

    Add ecr for getting images once published and deployed Add comment to refactor code once support for external redis is added

--- app/redis_connection.py
+++ app/redis_connection.py
@@ -2,6 +2,7 @@ import redis
 import os
 
 redis_conn = None
+# TODO: Remove once external (aws) redis script is in place
 if os.environ["REDIS_CREATE"] == "true":
     redis_conn = redis.StrictRedis(
                 host=os.environ["REDIS_HOST"],
