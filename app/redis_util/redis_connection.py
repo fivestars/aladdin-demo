e8a89dd7f4b4b4eb77baebbd4de0c7a57dc24668
Hope Yu <hope.yu@fivestars.com>
2017-10-02 13:58:42 -0700

    Restructure redis python directory

--- app/redis_util/redis_connection.py
+++ app/redis_util/redis_connection.py
@@ -8,4 +8,3 @@ if os.environ["REDIS_CREATE"] == "true":
                 host=os.environ["REDIS_HOST"],
                 port=os.environ["REDIS_PORT"],
             )
-    redis_conn.set('msg', '\n I can show you the world from Redis \n \n')
