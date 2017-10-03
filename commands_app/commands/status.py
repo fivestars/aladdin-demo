6823874d59bffca019ec7b3c44c676062636e926
Hope Yu <hope.yu@fivestars.com>
2017-10-03 11:59:47 -0700

    Add comments

--- commands_app/commands/status.py
+++ commands_app/commands/status.py
@@ -2,9 +2,11 @@ import socket
 
 def parse_args(sub_parser):
     subparser = sub_parser.add_parser("status", help="Report on the status of the application")
+    # register the function to be executed when command "status" is called
     subparser.set_defaults(func=print_status)
 
 def print_status(arg):
+    """ Prints the status of the aladdin-demo pod and the redis pod """
     if socket.gethostbyname("aladdin-demo"):
         print("Aladdin demo app up and ready")
     else:
