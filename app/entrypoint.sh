aaea9f3759ecac684a7917ec0a5d165275687496
Hope Yu <hope.yu@fivestars.com>
2018-02-14 17:30:13 -0800

    change entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # start our server through ddtrace-run, which will allow us to see apm tracing in datadog
-ddtrace-run uwsgi /home/config/uwsgi.yaml
+DATADOG_TRACE_DEBUG=true DATADOG_TRACE_AGENT_HOSTNAME=datadog-agent-apm.kube-system ddtrace-run uwsgi /home/config/uwsgi.yaml
