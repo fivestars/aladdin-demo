6a99c8a6218e8e91bde4361fa97153683b6f5826
hopeyu <31780240+hopeyu@users.noreply.github.com>
2018-03-06 16:28:54 -0800

    Datadog apm tracing (#20)
    * Add datadog apm tracing compatibility
    
    * Create monitoring documentation

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-uwsgi /config/uwsgi.yaml
+if $APM_ENABLED; then
+    echo "APM tracing enabled: $APM_APP_NAME"
+    # start our server through ddtrace-run, which will allow us to see apm tracing in datadog
+    ddtrace-run uwsgi /config/uwsgi.yaml
+else
+    echo "APM tracing disabled"
+    uwsgi /config/uwsgi.yaml
+fi
