89c1b8879d7ae591e004179a45004ca8929b25fc
Abhinav Gianey <abhinav.gianey@fivestars.com>
2018-06-11 17:49:25 -0700

    Fix up build script

--- build/build_docker.sh
+++ build/build_docker.sh
@@ -4,23 +4,11 @@ echo "Building aladdin-demo docker image (~30 seconds)"
 
 BUILD_PATH="$(cd "$(dirname "$0")"; pwd)"
 PROJ_ROOT="$(cd "$BUILD_PATH/.." ; pwd)"
-PRINT_ONLY="${PRINT_ONLY:-false}"
-HASH="${HASH:-local}"
-ALL="${ALL:-false}"
-
-print_only_cmd_wrapper() {
-    typeset cmd="$1"
-    echo "$cmd"
-    if ! $PRINT_ONLY; then
-        ${cmd}
-    fi
-}
 
 docker_build() {
     typeset name="$1" dockerfile="$2" context="$3"
     TAG="$name:${HASH}"
-    build_cmd="docker build -t $TAG -f $dockerfile $context"
-    print_only_cmd_wrapper "$build_cmd"
+    docker build -t $TAG -f $dockerfile $context
 }
 cd "$PROJ_ROOT"
 
